153c6e61ff42cd7963a194c6c805701e02263ce4
codedhola <codedhola2000@gmail.com>
2022-09-07 00:54:15 +0100

    added markups

--- server.js
+++ server.js
@@ -5,21 +5,64 @@ const express = require("express");
 
 const app = express();
 app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+
+// ROUTING
+app.get("/", (req, res) => {
+    res.status(200).send("THIS IS THE HOMEPAGE FOR MY APP\u{1F605}");
+});
+
+app.get("/index", (req, res) => {
+    const filePath = path.join(__dirname, "views", "index.html");
+    // FILE RENDERED SUCCESSFULLY
+    res.sendFile(filePath);
+});
+
+app.get("/about", (req, res) => {
+    const filePath = path.join(__dirname, "views", "about.html");
+    // FILE RENDERED SUCCESSFULLY
+    res.sendFile(filePath);
+});
+
+app.get("/scholars", (req, res) => {
+    const filePath = path.join(__dirname, "views", "scholars.html");
+    // FILE RENDERED SUCCESSFULLY
+    res.sendFile(filePath);
+});
+
+app.get("/register", (req, res) => {
+    const filePath = path.join(__dirname, "views", "register.html");
+    // FILE RENDERED SUCCESSFULLY
+    res.sendFile(filePath);
+});
+
+app.get("/recommend", (req, res) => {
+    const filePath = path.join(__dirname, "views", "recommend.html");
+    // FILE RENDERED SUCCESSFULLY
+    res.sendFile(filePath);
+});
+
+app.post("/recommend", (req, res) => {
+    const scholars = req.body;
+    console.log(scholars);
+    res.redirect("scholars")
+});
+
+
+
+
+
+
+
+/**
+ * EXERCISES 1 ---- 
 
 const filePath = path.join(__dirname, "data", "db.json");
 
 let data = fs.readFileSync(filePath, "utf-8");
 
 
-app.get("/", (req, res) => {
-    res.status(200).send("Running server initiated ");
-});
-
-app.get("/about", (req, res) => {
-    res.status(200).send("<form action='/request' method='POST'><input type='text' name='user' /><button>submit</button>");
-});
-
-app.post("/request", (req, res) => { 
+app.post("/api/request", (req, res) => { 
     let user = req.body.user;
     const storeData = JSON.parse(data);
    storeData.push(user);
@@ -30,7 +73,7 @@ app.post("/request", (req, res) => {
     res.send({userName: user});
 });
 
-app.get("/data", (req, res) => {
+app.get("/api/data", (req, res) => {
     const dataBase = JSON.parse(data);
     let output = "<ul>"
     for(const test of dataBase){
@@ -39,7 +82,13 @@ app.get("/data", (req, res) => {
     output += "</ul>";
 
     res.send(output);
-})
+});
+
+ */
+
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

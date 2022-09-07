01aa47b7531661d04cbeaf0613543016d2245a5c
codedhola <codedhola2000@gmail.com>
2022-09-07 13:43:34 +0100

    added ejs to practise

--- server.js
+++ server.js
@@ -4,8 +4,99 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs");
+
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.use(express.static("public"));
+
+// READ FILE 
+const filePath = path.join(__dirname, "data", "db.json");
+const fileData = fs.readFileSync(filePath,"utf-8");
+
+// ROUTING
+app.get("/", (req, res) => {
+    res.status(200).send("THIS IS THE HOMEPAGE FOR MY APP\u{1F605}");
+});
+
+app.get("/index", (req, res) => {
+    res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/scholars", (req, res) => {
+    res.render("scholars");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/recommend", (req, res) => {
+    res.render("recommend");
+});
+
+app.post("/recommend", (req, res) => {
+    const scholars = req.body;
+    const data = JSON.parse(fileData);
+    data.push(scholars);
+    console.log(data);
+    fs.writeFile(filePath, JSON.stringify(data) , (error) => {
+        if(error) throw error 
+    })
+
+    res.redirect("scholars");
+});
+
+
+
+
+
+
+
+/**
+ * EXERCISES 1 ---- 
+ * 
+ * 
+ * --------------------------------------------------------------------------------------------
+
+const filePath = path.join(__dirname, "data", "db.json");
+
+let data = fs.readFileSync(filePath, "utf-8");
+
+
+app.post("/api/request", (req, res) => { 
+    let user = req.body.user;
+    const storeData = JSON.parse(data);
+   storeData.push(user);
+   data = JSON.stringify(data); 
+    fs.writeFile(filePath, JSON.stringify(storeData), "utf-8", (error) => {
+        if(error) throw error
+    })
+    res.send({userName: user});
+});
+
+app.get("/api/data", (req, res) => {
+    const dataBase = JSON.parse(data);
+    let output = "<ul>"
+    for(const test of dataBase){
+        output += `<li> ${test} </li>`
+    }
+    output += "</ul>";
+
+    res.send(output);
+});
+
+------------------------------------------------------------------------------------------
+
+//  EXERCISE 2
+
+------------------------------------------------------------------------------------------
 
 // ROUTING
 app.get("/", (req, res) => {
@@ -44,45 +135,21 @@ app.get("/recommend", (req, res) => {
 
 app.post("/recommend", (req, res) => {
     const scholars = req.body;
-    console.log(scholars);
-    res.redirect("scholars")
-});
-
-
-
-
-
-
-
-/**
- * EXERCISES 1 ---- 
-
-const filePath = path.join(__dirname, "data", "db.json");
-
-let data = fs.readFileSync(filePath, "utf-8");
-
-
-app.post("/api/request", (req, res) => { 
-    let user = req.body.user;
-    const storeData = JSON.parse(data);
-   storeData.push(user);
-   data = JSON.stringify(data); 
-    fs.writeFile(filePath, JSON.stringify(storeData), "utf-8", (error) => {
-        if(error) throw error
+    const data = JSON.parse(fileData);
+    data.push(scholars);
+    console.log(data);
+    fs.writeFile(filePath, JSON.stringify(data) , (error) => {
+        if(error) throw error 
     })
-    res.send({userName: user});
+
+    res.redirect("scholars");
 });
 
-app.get("/api/data", (req, res) => {
-    const dataBase = JSON.parse(data);
-    let output = "<ul>"
-    for(const test of dataBase){
-        output += `<li> ${test} </li>`
-    }
-    output += "</ul>";
 
-    res.send(output);
-});
+----------------------------------------------------------------------------------------------------------
+
+
+
 
  */
 
